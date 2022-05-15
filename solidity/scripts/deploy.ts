b3e908bcaa8540733dad16828f0c9badbcf5961b
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2022-05-15 01:12:22 -0700

    moved to TS

--- solidity/scripts/deploy.ts
+++ solidity/scripts/deploy.ts
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
 
-async function main() {
-    const Contract = await hre.ethers.getContractFactory("Contract");
-    const contract = await Contract.deploy();
+const main = async (): Promise<void> => {
+    const Contract: any = await hre.ethers.getContractFactory("Contract");
+    const contract: any = await Contract.deploy();
     await contract.deployed();
 
     console.log("Contract deployed to:", contract.address);
