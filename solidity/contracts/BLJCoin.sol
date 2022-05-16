03d009f7dc93b23070917898992b295c388599f7
BrandonD <32801607+Wolf-111@users.noreply.github.com>
2022-05-16 00:23:32 -0700

    file renamed

--- solidity/contracts/BLJCoin.sol
+++ solidity/contracts/BLJCoin.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BLJCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Blackjack Coin", "BLJ") {
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 _totalSupply) ERC20("Blackjack Coin", "BLJ") {
+        _mint(msg.sender, _totalSupply);
     }
 }
