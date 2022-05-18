a0326da84a09ccbf99eba88df052052dd56cb586
BrandonD <dalbecb@gmail.com>
2022-05-17 17:22:34 -0700

    added modifiers & playHand() (WIP)

--- solidity/contracts/Blackjack.sol
+++ solidity/contracts/Blackjack.sol
@@ -16,9 +16,20 @@ contract Blackjack is BLJCoin{
         dealer = msg.sender;
     }
 
-    function enterTournament() public {
-        require(msg.sender != dealer, "Error: Dealer can't enter tournament");
+    event BeginHand(address player, uint betAmount);
+
+    modifier onlyExistingPlayers {
+        require(players[msg.sender] == true, "Error: Player has not entered the tournament");
+        _;
+    }
+
+    modifier onlyNewPlayers {
         require(players[msg.sender] == false, "Error: Player has already entered");
+        _;
+    }
+
+    function enterTournament() onlyNewPlayers public {
+        require(msg.sender != dealer, "Error: Dealer can't enter tournament");
         require(playerBalances[msg.sender] == 0, "Error: Player already has coins");
         players[msg.sender] = true;
 
@@ -26,6 +37,23 @@ contract Blackjack is BLJCoin{
         uint amount = 10000;
         BLJCoin.fundPlayer(msg.sender, amount);
         playerBalances[msg.sender] += amount;
+    }
+
+    function playHand(uint _betAmount) onlyExistingPlayers public {
+        require(playerBalances[msg.sender] >= _betAmount, "Error: Not enough funds to place bet");
+        
+        emit BeginHand(msg.sender, _betAmount);
+
+        /*
+        1. Player places bet
+        2. Player & dealer are randomly dealt 2 cards each (player can only see 1 of the dealers cards)
+        3. Player given option to double down or not
+        4. Player can hit or stay
+        5. If stay, reveal dealers hidden card & keep hitting until values are greater than or equal to
+           the player's cards. If exceeds 21, player wins.
+        6. If hit, randomly add card to players hand. If exceeds 21, dealer wins.
+        7. Whichever person has the bigger hand wins, if exceeds 21, other player wins.
+        */
     }
 
     function giveAwards() public {
